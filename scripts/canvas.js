4a3e84205c6248a8e0bc64723d2b36b641c2f731
Ricky Chon <rickychon99@gmail.com>
2021-02-24 01:34:00 -0500

    Add modifiable noise size range input

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -5,32 +5,48 @@ let ctx = canvas.getContext('2d');
 let image = ctx.createImageData(canvas.width, canvas.height);
 let data = image.data;
 
-let start = Date.now();
+let noiseSize = document.getElementById('noise-size').value;
+generateNoise();
 
-noise.seed(Math.random());
+let ctrlPanelData = document.getElementById('ctrl-form');
+ctrlPanelData.addEventListener('submit', onSubmit, false);
 
-for(let x = 0; x < canvas.width; x++)
+function onSubmit(e)
 {
-    for(let y = 0; y < canvas.height; y++)
-    {
-        // Noise functions return values between -1 and 1
-
-        let value = noise.simplex2(x / 50, y / 50);
-        value *= 256;
-
-        let cell = (x + y * canvas.width) * 4;
-        data[cell] = data[cell + 1] = data[cell + 2] = value;
-        // data[cell] += Math.max(0, (25 - value) * 8); // rgb
-        data[cell + 3] = 255; // alpha
-    }
+    e.preventDefault();
+    noiseSize = e.target[0].value;
+    generateNoise();
 }
 
-ctx.fillColor = 'black';
-ctx.fillRect(0, 0, canvas.width, canvas.height);
-ctx.putImageData(image, 0, 0);
+function generateNoise()
+{
+    let start = Date.now();
 
-let end = Date.now();
-let caption = document.getElementById('caption');
+    noise.seed(Math.random());
 
-console.log('Rendered in ' + (end - start) + ' ms');
-caption.textContent = (`Rendered in ${end - start} ms`);
+    for(let x = 0; x < canvas.width; x++)
+    {
+        for(let y = 0; y < canvas.height; y++)
+        {
+            // Noise functions return values between -1 and 1
+
+            let value = noise.simplex2(x / noiseSize, y / noiseSize);
+            value *= 256;
+
+            let cell = (x + y * canvas.width) * 4;
+            data[cell] = data[cell + 1] = data[cell + 2] = value;
+            // data[cell] += Math.max(0, (25 - value) * 8); // rgb
+            data[cell + 3] = 255; // alpha
+        }
+    }
+
+    ctx.fillColor = 'black';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.putImageData(image, 0, 0);
+
+    let end = Date.now();
+    let caption = document.getElementById('caption');
+
+    console.log('Rendered in ' + (end - start) + ' ms');
+    caption.textContent = (`Rendered in ${end - start} ms`);
+}
